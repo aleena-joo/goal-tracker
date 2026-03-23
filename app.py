de89103faf268b369dbbf95e011e5b6b8e26a747
aleena-joo <aleenajoseph115@gmail.com>
2026-03-23 18:07:17 +0530

    Fix port issue

--- app.py
+++ app.py
@@ -111,4 +111,8 @@ def delete(id):
     return redirect("/")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
