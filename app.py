80060cdc1dd6518a56ea19f40aee2470989b887c
aleena-joo <aleenajoseph115@gmail.com>
2026-03-23 18:19:42 +0530

    fix error

--- app.py
+++ app.py
@@ -110,9 +110,5 @@ def delete(id):
     conn.close()
     return redirect("/")
 
-if __name__ == "__main__":
-    import os
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+
